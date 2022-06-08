3d37b0a204c4ccd3809433b35f8c9a4371ce69cb
badmaru76 <88932778+badmaru76@users.noreply.github.com>
2022-06-09 00:25:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,113 @@
-print("Hello")
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showerror, showwarning, showinfo
+
+current_value = 0 #start
+
+
+def create_input_frame(container):
+
+    frame = ttk.Frame(container)
+
+    # grid layout for the input frame
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=3)
+
+    # Find what
+    ttk.Label(frame, text='Find what:').grid(column=0, row=0, sticky=tk.W)
+    keyword = ttk.Entry(frame, width=20)
+    keyword.focus()
+    keyword.grid(column=1, row=0, sticky=tk.W)
+
+    # Replace with:
+    ttk.Label(frame, text='Replace with:').grid(column=0, row=1, sticky=tk.W)
+    replacement = ttk.Entry(frame, width=30)
+    replacement.grid(column=1, row=1, sticky=tk.W)
+
+    # Match Case checkbox
+    match_case = tk.StringVar()
+    match_case_check = ttk.Checkbutton(
+        frame,
+        text='Match case',
+        variable=match_case,
+        command=lambda: print(match_case.get()))
+    match_case_check.grid(column=0, row=2, sticky=tk.W)
+
+    # Wrap Around checkbox
+    wrap_around = tk.StringVar()
+    wrap_around_check = ttk.Checkbutton(
+        frame,
+        variable=wrap_around,
+        text='Wrap around',
+        command=lambda: print(wrap_around.get()))
+    wrap_around_check.grid(column=0, row=3, sticky=tk.W)
+
+    #  slider
+    slider = ttk.Scale(
+        frame,
+        from_=0,
+        to=100,
+        orient='horizontal',  # vertical
+        command=slider_changed,
+        variable=current_value
+    )
+
+    slider.grid(column=0, row=3, sticky=tk.W)
+
+
+
+    for widget in frame.winfo_children():
+        widget.grid(padx=0, pady=5)
+
+    return frame
+
+
+def create_button_frame(container):
+    frame = ttk.Frame(container)
+
+    frame.columnconfigure(0, weight=1)
+
+    ttk.Button(frame, text='Find Next',command=lambda: showwarning(
+        title='Warning',
+        message='This is a warning message.')).grid(column=0, row=0)
+    ttk.Button(frame, text='Replace').grid(column=0, row=1)
+    ttk.Button(frame, text='Replace All').grid(column=0, row=2)
+    ttk.Button(frame, text='Cancel').grid(column=0, row=3)
+
+    for widget in frame.winfo_children():
+        widget.grid(padx=0, pady=3)
+
+    return frame
+
+
+def create_main_window():
+
+    # root window
+    root = tk.Tk()
+    root.title('Replace')
+    root.geometry('400x150')
+    root.resizable(False, False)
+    # windows only (remove the minimize/maximize button)
+    root.attributes('-toolwindow', True)
+
+    # layout on the root window
+    root.columnconfigure(0, weight=4)
+    root.columnconfigure(1, weight=1)
+
+    # slider current value
+    current_value = tk.DoubleVar()
+
+    input_frame = create_input_frame(root)
+    input_frame.grid(column=0, row=0)
+
+    button_frame = create_button_frame(root)
+    button_frame.grid(column=1, row=0)
+
+    root.mainloop()
+
+
+def slider_changed(event):
+    print(event)#slider.get())
+
+if __name__ == "__main__":
+    create_main_window()
